74d43af7bdd9074bbc03c103a7a4fceb5f38b951
kekubezos <keknorgbey@yahoo.com>
2022-09-27 22:19:36 +0000

    Adding Band Generator project

---
+++ Day-1/band-generator.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Generator.")
+
+city = input("What city did you grow up in?\n")
+
+pet = input("What is the name of your favorite pet?\n")
+
+band_name = city + " " + pet
+print("Your band is called " + band_name)
